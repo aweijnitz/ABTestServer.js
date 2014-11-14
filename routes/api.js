2d1a2b37582e668f040e62278081e7a80e6650fe
Anders Weijnitz <anders.weijnitz@my-email.com>
2014-11-14 18:07:49 +0100

    Tests must now be created before it is possible to submit counts

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,20 @@ var db = express.db;
 if(db == null || typeof db == 'undefined')
     console.log("NO DB");
 
+
+var testCounter = 0;
+
 // Make empty new AB test object, with given test id
 var newTest = function (testID) {
     return { testID: testID, variantViews: [0, 0], conversions: [0 ,0] }
+};
+
+
+// Create new tst and save. Returns test.
+var createTest = function() {
+    var tst = newTest(('t'+testCounter++)+(new Date().getTime()));
+    db.upsert(tst);
+    return tst;
 };
 
 // Fetch test object from DB
@@ -23,7 +34,7 @@ var incView = function(testID, variant) {
     if(!o)
         o = newTest(testID);
     o.variantViews[variant]++;
-    db.upsert(o);
+    db.update(o);
     return o;
 };
 
@@ -34,14 +45,14 @@ var incConversion = function(testID, variant) {
     if(!o)
         o = newTest(testID);
     o.conversions[variant]++;
-    db.upsert(o);
+    db.update(o);
     return o;
 };
 
 
 /* GET test status. */
 router.get('/:testid?', function(req, res) {
-    var id = req.params.testid; // can be undefined, then all running tests listed
+    var id = req.params.testid; // might be undefined, then all running tests listed
     var o = getObj(id);
     if(!!o) {
         o.stats = {
@@ -56,9 +67,19 @@ router.get('/:testid?', function(req, res) {
 });
 
 
+router.put('/', function(req, res) {
+    res.send({ testID: createTest().testID });
+});
+
 /* POST increase count on a given test variant [0 or 1]. */
 router.post('/view/:testid/:variant', function(req, res) {
-    res.send(incView(req.params.testid, req.params.variant)).end()
+    var o = null;
+    try {
+        o = incView(req.params.testid, req.params.variant);
+        res.send(o).end()
+    } catch(err) {
+        res.status(404).send({ error: -1, msg: 'No such test '+req.params.testid}).end();
+    }
 });
 
 
--- test/test-REST-api.js
+++ test/test-REST-api.js
@@ -16,32 +16,50 @@ describe('Basic API', function () {
     var server = null;
 
     var port = ((appConf.server.port || process.env.PORT) || 8080);
-    var baseURL = 'http://127.0.0.1:' + port + '/tests/';
+    var baseURL = 'http://127.0.0.1:' + port + '/tests';
 
-    var testID = 'test213';
     var variantName0 = '0';
     var variantName1 = '1';
-    var variant0url = baseURL + 'view/' + testID + '/' + variantName0;
-    var variant1url = baseURL + 'view/' + testID + '/' + variantName1;
+    var variant0url = function (testID) {
+        return baseURL + '/view/' + testID + '/' + variantName0;
+    };
+    var variant1url = function (testID) {
+        return baseURL + '/view/' + testID + '/' + variantName1;
+    };
 
-    var test1url = baseURL + testID;
-    var convert0url = baseURL + 'convert/' + testID + '/' + variantName0;
-    var convert1url = baseURL + 'convert/' + testID + '/' + variantName1;
+    var test1url = function (testID) {
+        return baseURL + '/' + testID;
+    };
+    var convert0url = function (testID) {
+        return baseURL + '/convert/' + testID + '/' + variantName0;
+    };
+    var convert1url = function (testID) {
+        return baseURL + '/convert/' + testID + '/' + variantName1;
+    };
+
+    // Test helper
+    var newObj = function newObj(cb) {
+        return (request
+            .put(baseURL)
+            .end(function (res) {
+                cb(res.body.testID);
+            }));
+    };
 
     // start a test server
-    before(function() {
+    before(function () {
         server = app.listen(app.get('port'), function () {
             //logger.info('Server listening on port ' + server.address().port);
         });
     });
 
-    after(function() {
+    after(function () {
         server.close();
     });
 
     it('Should return empty object when GETting a non-existent test', function (done) {
         request
-            .get(test1url)
+            .get(test1url())
             .end(function (res) {
                 //  console.log(util.inspect(res.body));
                 (res.status).should.equal(200);
@@ -50,66 +68,100 @@ describe('Basic API', function () {
             });
     });
 
-    it('Should return test object with status when GETting a existent test', function (done) {
-
+    it('Should return testID when PUTing to ' + baseURL, function (done) {
         request
-            .post(variant1url)
+            .put(baseURL)
             .end(function (res) {
+                //console.log(util.inspect(res.body));
                 (res.status).should.equal(200);
-
-                request
-                    .post(variant1url)
-                    .end(function (res) {
-                        (res.status).should.equal(200);
-
-                        request
-                            .post(convert1url)
-                            .end(function (res) {
-                                (res.status).should.equal(200);
-
-                                request
-                                    .get(test1url)
-                                    .end(function (res) {
-                                        // console.log(util.inspect(res.body, { colors: true }));
-
-                                        (res.status).should.equal(200);
-                                        (res.body.variantViews[1]).should.equal(2);
-                                        (res.body.conversions[1]).should.equal(1);
-
-                                        (res.body.stats.isSignificant).should.be.false;
-                                        (res.body.stats.isSignificantForB).should.be.false;
-                                        (res.body.stats.isSignificantForA).should.be.false;
-
-                                        done();
-                                    });
-                            });
-                    });
+                res.body.testID.should.be.ok;
+                done();
             });
+    });
+
+
+    it('Should return test object with status when GETting a existent test', function (done) {
+        newObj(function (testId) {
+
+            request
+                .post(variant1url(testId))
+                .end(function (res) {
+                    (res.status).should.equal(200);
+
+                    request
+                        .post(variant1url(testId))
+                        .end(function (res) {
+                            (res.status).should.equal(200);
+
+                            request
+                                .post(convert1url(testId))
+                                .end(function (res) {
+                                    (res.status).should.equal(200);
+
+                                    request
+                                        .get(test1url(testId))
+                                        .end(function (res) {
+                                            // console.log(util.inspect(res.body, { colors: true }));
+
+                                            (res.status).should.equal(200);
+                                            (res.body.variantViews[1]).should.equal(2);
+                                            (res.body.conversions[1]).should.equal(1);
+
+                                            (res.body.stats.isSignificant).should.be.false;
+                                            (res.body.stats.isSignificantForB).should.be.false;
+                                            (res.body.stats.isSignificantForA).should.be.false;
+
+                                            done();
+                                        });
+                                });
+                        });
+                });
+        });
+
     });
 
 
     it('Should add one to view count when POSTing a variant view', function (done) {
-        request
-            .post(variant0url)
-            .end(function (res) {
-                (res.status).should.equal(200);
-                res.body.testID.should.equal(testID);
-                (res.body.variantViews[0]).should.equal(1);
-                done();
-            });
+        newObj(function (testId) {
+            request
+                .post(variant0url(testId))
+                .end(function (res) {
+                    console.log(util.inspect(res.body) + ' '+testId);
+
+                    (res.status).should.equal(200);
+                    res.body.testID.should.equal(testId);
+                    (res.body.variantViews[0]).should.equal(1);
+                    done();
+                });
+        });
+
     });
 
 
-    it('Should add one to convert count when POSTing a variant conversion', function (done) {
+    it('Should FAIL when trying to add to non-existing test', function (done) {
+        var testId = 'failMe';
         request
-            .post(convert0url)
+            .post(variant0url(testId))
             .end(function (res) {
-                //console.log(util.inspect(res.body));
-                (res.status).should.equal(200);
-                res.body.testID.should.equal(testID);
-                (res.body.conversions[0]).should.equal(1);
+
+                (res.status).should.equal(404);
+                (res.body.error).should.equal(-1);
                 done();
             });
+
+    });
+
+    it('Should add one to convert count when POSTing a variant conversion', function (done) {
+        newObj(function (testId) {
+            request
+                .post(convert0url(testId))
+                .end(function (res) {
+                    (res.status).should.equal(200);
+                    res.body.testID.should.equal(testId);
+                    (res.body.conversions[0]).should.equal(1);
+                    done();
+                });
+        });
     });
 
 
